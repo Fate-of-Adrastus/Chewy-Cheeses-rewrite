96e2c6e7fb97b76c3db6c91239e00778980a0e09
Fate of Adrastus <kovatstoni@gmail.com>
2026-05-02 22:17:59 +0200

    Fixed block being placed even when it shouldn't be.

--- src/main/java/com/fateofadrastus/chewy_cheeses/util/MixinUtils.java
+++ src/main/java/com/fateofadrastus/chewy_cheeses/util/MixinUtils.java
@@ -1,9 +1,12 @@
 package com.fateofadrastus.chewy_cheeses.util;
 
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -11,33 +14,53 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.shapes.CollisionContext;
 
 public class MixinUtils {
-    public static InteractionResult placeCheese(BlockPlaceContext placeContext, Block blockToPlace){
-        if (placeContext == null || !placeContext.canPlace()) return InteractionResult.FAIL;
 
-        BlockState blockState = blockToPlace.getStateForPlacement(placeContext);
+    public static InteractionResult placeCheese(BlockPlaceContext placeContext, Block blockToPlace){
+        Level level = placeContext.getLevel();
+        if (!blockToPlace.isEnabled(level.enabledFeatures()) || !placeContext.canPlace()) return InteractionResult.FAIL;
+
+        BlockState blockState = MixinUtils.getPlacementState(placeContext, blockToPlace);
         if (blockState == null ) return  InteractionResult.FAIL;
 
         BlockPos blockPos = placeContext.getClickedPos();
-        Level world = placeContext.getLevel();
-        if (!world.setBlock(blockPos, blockState, 11))
+        if (!level.setBlock(blockPos, blockState, 11))
             return InteractionResult.FAIL;
 
         ItemStack itemStack = placeContext.getItemInHand();
-        Player playerEntity = placeContext.getPlayer();
-        SoundType blockSoundGroup = blockToPlace.getSoundType(blockToPlace.defaultBlockState(),world,blockPos,playerEntity);
+        Player player = placeContext.getPlayer();
+        SoundType soundType = blockToPlace.getSoundType(blockToPlace.defaultBlockState(),level,blockPos,player);
 
-        itemStack.consume(1, playerEntity);
-        world.gameEvent(GameEvent.BLOCK_PLACE, blockPos, GameEvent.Context.of(playerEntity, blockState));
-        world.playSound(playerEntity,
+        BlockItem.updateCustomBlockEntityTag( level, player,blockPos, itemStack);
+        blockToPlace.setPlacedBy(level, blockPos, blockState, player, itemStack);
+        if (player instanceof ServerPlayer) {
+            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer)player, blockPos, itemStack);
+        }
+
+        itemStack.consume(1, player);
+        level.gameEvent(GameEvent.BLOCK_PLACE, blockPos, GameEvent.Context.of(player, blockState));
+        level.playSound(
+                player,
                 blockPos,
-                blockSoundGroup.getPlaceSound(),
+                soundType.getPlaceSound(),
                 SoundSource.BLOCKS,
-                blockSoundGroup.getVolume(),
-                blockSoundGroup.getPitch()
+                (soundType.getVolume() + 1.0F) / 2.0F,
+                soundType.getPitch() * 0.8F
         );
 
-        return InteractionResult.SUCCESS;
+        return InteractionResult.sidedSuccess(level.isClientSide);
     }
+    private static BlockState getPlacementState(BlockPlaceContext placeContext, Block blockToPlace){
+        BlockState blockstate = blockToPlace.getStateForPlacement(placeContext);
+        return blockstate != null && canPlace(placeContext, blockstate) ? blockstate : null;
+    }
+    private static boolean canPlace(BlockPlaceContext context, BlockState state) {
+        Player player = context.getPlayer();
+        CollisionContext collisioncontext = player == null ? CollisionContext.empty() : CollisionContext.of(player);
+        return (state.canSurvive(context.getLevel(), context.getClickedPos()))
+                && context.getLevel().isUnobstructed(state, context.getClickedPos(), collisioncontext);
+    }
+
 }
